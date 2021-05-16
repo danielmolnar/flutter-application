1776d926a1b85e85cb070fa9a626d710310d53aa
Daniel Molnar <daniel-molnar@web.de>
2021-05-16 15:23:15 +0200

    added customized appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'get_hex_color.dart';
 import 'typewriter.dart';
 import 'create_material_color.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,30 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: createMaterialColor(Color(0xFF14022b)),
         ),
-        home: MyColumn());
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+            centerTitle: true,
+            title: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text('\$ job application ',
+                  style: GoogleFonts.pressStart2p(
+                      textStyle: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          decoration: TextDecoration.none))),
+            )),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(alignment: Alignment.center, child: MyColumn()),
+        ));
   }
 }
 
