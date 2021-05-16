362b62884a70cd492f82d334216116d014d00115
Daniel Molnar <daniel-molnar@web.de>
2021-05-16 15:13:43 +0200

    added color functions for MaterialColor and get hex color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'typewriter.dart';
+import 'create_material_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Job Application',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: createMaterialColor(Color(0xFF14022b)),
         ),
         home: TypeWriter());
   }
